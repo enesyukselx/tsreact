2bcbcd09be149c84dc0f6e9bf744ccbf546993b5
Enes Yüksel <enesyuksel9903@gmail.com>
2023-09-05 15:21:06 +0300

    Creating routes with react-router-dom

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import {
+    Outlet,
+    Link,
+    RouterProvider,
+    createBrowserRouter,
+} from "react-router-dom";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import "./App.css";
+import ErrorBoundary from "./Pages/ErrorBoundary/ErrorBoundary";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <div>
+                <Layout />
+                <Container className="mt-4">
+                    <Outlet />
+                </Container>
+            </div>
+        ),
+        errorElement: <ErrorBoundary />,
+        children: [
+            {
+                path: "/",
+                element: <div>Home</div>,
+            },
+            {
+                path: "page1",
+                element: <div>Page 1</div>,
+            },
+            {
+                path: "page2",
+                element: <div>Page 2</div>,
+            },
+        ],
+    },
+]);
+
+export function Layout() {
+    return (
+        <Navbar bg="dark" data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand>React</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link>
+                        <Link to="/">Home</Link>
+                    </Nav.Link>
+                    <Nav.Link>
+                        <Link to="/page1">Page 1</Link>
+                    </Nav.Link>
+                    <Nav.Link>
+                        <Link to="/page2">Page 2</Link>
+                    </Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+export default App;
