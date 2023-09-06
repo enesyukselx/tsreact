40b17f51da8ef4f4d80867fb216eadc1b24dcbd7
Enes Yüksel <enesyuksel9903@gmail.com>
2023-09-06 11:18:00 +0300

    Update Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from "react";
 import Layout from "./Components/Layout/Layout";
 import ErrorBoundary from "./Pages/ErrorBoundary/ErrorBoundary";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
@@ -6,8 +7,13 @@ import "react-toastify/dist/ReactToastify.css";
 
 import "./App.css";
 import UserContextProvider from "./Components/UserContext/UserContextProvider";
+import MustBeGuest from "./Components/Auth/MustBeGuest";
+import MustBeLogin from "./Components/Auth/MustBeLogin";
 
 import Home from "./Pages/Home/Home";
+const Login = lazy(() => import("./Pages/Login/Login"));
+const Account = lazy(() => import("./Pages/Account/Account"));
+const Logout = lazy(() => import("./Pages/Logout/Logout"));
 
 const router = createBrowserRouter([
     {
@@ -26,6 +32,36 @@ const router = createBrowserRouter([
             {
                 path: "page2",
                 element: <div>Page 2</div>,
+            },
+            {
+                path: "login",
+                element: (
+                    <MustBeGuest>
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Login />
+                        </Suspense>
+                    </MustBeGuest>
+                ),
+            },
+            {
+                path: "account",
+                element: (
+                    <MustBeLogin>
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Account />
+                        </Suspense>
+                    </MustBeLogin>
+                ),
+            },
+            {
+                path: "logout",
+                element: (
+                    <MustBeLogin>
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Logout />
+                        </Suspense>
+                    </MustBeLogin>
+                ),
             },
         ],
     },
