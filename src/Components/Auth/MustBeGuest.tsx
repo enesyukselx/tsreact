dd85d03cf1859984cbdeee27cd44009e53dbfa5b
Enes Yüksel <enesyuksel9903@gmail.com>
2023-09-06 11:14:29 +0300

    Create Auth Components
    Create MustBeGuest.tsx and MustBeLogin.tsx

---
+++ src/Components/Auth/MustBeGuest.tsx
@@ -0,0 +1,19 @@
+import { ReactNode, useContext, useEffect } from "react";
+import UserContext from "../UserContext/UserContext";
+import { useNavigate } from "react-router-dom";
+
+const MustBeGuest = ({ children }: { children: ReactNode }) => {
+    const navigate = useNavigate();
+    const { isLogin } = useContext(UserContext);
+
+    useEffect(() => {
+        if (isLogin) {
+            navigate("/");
+            return;
+        }
+    });
+
+    return children;
+};
+
+export default MustBeGuest;
