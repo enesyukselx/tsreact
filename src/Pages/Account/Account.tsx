dc0ecb16acefe41a300eb2b958f1e03da7cd4d77
Enes Yüksel <enesyuksel9903@gmail.com>
2023-09-06 11:15:57 +0300

    Create Login, Logout and Account Components

---
+++ src/Pages/Account/Account.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const Account = () => {
+    return (
+        <Container>
+            <h3>Account</h3>
+        </Container>
+    );
+};
+
+export default Account;
---
+++ src/Pages/Logout/Logout.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const Logout = () => {
+    return (
+        <Container>
+            <h3>Logout</h3>
+        </Container>
+    );
+};
+
+export default Logout;
