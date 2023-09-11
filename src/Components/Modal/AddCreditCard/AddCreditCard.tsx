60db13c6af613ff3a92944f474310e94dd1ff4bb
Enes Yüksel <enesyuksel9903@gmail.com>
2023-09-11 15:57:46 +0300

    Create AddCreditCard

---
+++ src/Components/Modal/AddCreditCard/AddCreditCard.tsx
@@ -0,0 +1,174 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import useUser from "../../../Hooks/useUser";
+import useModal from "../../../Hooks/useModal";
+import Cards from "react-credit-cards-2";
+import "react-credit-cards-2/dist/es/styles-compiled.css";
+import { Form, Button } from "react-bootstrap";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z, ZodType } from "zod";
+import { toast } from "react-toastify";
+
+interface IFormValues {
+    cvc: string;
+    expiry: string;
+    name: string;
+    number: string;
+}
+
+const AddCreditCard = () => {
+    const { toggleModal } = useModal();
+    const { user, addCreditCard } = useUser();
+
+    const schema: ZodType<IFormValues> = z.object({
+        cvc: z.string().min(3).max(3),
+        expiry: z.string().min(4).max(4),
+        name: z.string().min(1).max(50),
+        number: z.string().min(16).max(16),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({ resolver: zodResolver(schema) });
+
+    const onSubmit = (data: IFormValues): void => {
+        const newCard = {
+            id: Math.floor(Math.random() * 1000),
+            userId: user?.id,
+            cvc: data.cvc,
+            expirationDate: data.expiry,
+            name: data.name,
+            number: data.number,
+        };
+
+        addCreditCard(newCard);
+        toggleModal();
+        toast.success("Credit card added successfully");
+    };
+
+    const [state, setState] = useState({
+        cvc: "",
+        expiry: "",
+        focus: "",
+        name: "",
+        number: "",
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setState((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleInputFocus = (e) => {
+        setState((prev) => ({ ...prev, focus: e.target.name }));
+    };
+
+    return (
+        <div>
+            <Cards
+                number={state.number}
+                expiry={state.expiry}
+                cvc={state.cvc}
+                name={state.name}
+                focused={state.focus}
+            />
+            <Form onSubmit={handleSubmit(onSubmit)} className="mt-4">
+                <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                >
+                    <Form.Label>Card Number</Form.Label>
+                    <Form.Control
+                        {...register("number")}
+                        type="number"
+                        name="number"
+                        placeholder="Card Number"
+                        value={state.number}
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                        isInvalid={!!errors.number}
+                        required
+                    />
+                    {errors.number && (
+                        <Form.Text className="text-danger">
+                            {errors.number.message}
+                        </Form.Text>
+                    )}
+                </Form.Group>
+                <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                >
+                    <Form.Label>Expiry Date (MM/YY)</Form.Label>
+                    <Form.Control
+                        {...register("expiry")}
+                        type="number"
+                        name="expiry"
+                        placeholder="MM/YY"
+                        value={state.expiry}
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                        isInvalid={!!errors.expiry}
+                        required
+                    />
+                    {errors.expiry && (
+                        <Form.Text className="text-danger">
+                            {errors.expiry.message}
+                        </Form.Text>
+                    )}
+                </Form.Group>
+                <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                >
+                    <Form.Label>CVC</Form.Label>
+                    <Form.Control
+                        {...register("cvc")}
+                        type="number"
+                        name="cvc"
+                        placeholder="CVC"
+                        value={state.cvc}
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                        isInvalid={!!errors.cvc}
+                        required
+                    />
+                    {errors.cvc && (
+                        <Form.Text className="text-danger">
+                            {errors.cvc.message}
+                        </Form.Text>
+                    )}
+                </Form.Group>
+                <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                >
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        {...register("name")}
+                        type="text"
+                        name="name"
+                        placeholder="John Doe"
+                        value={state.name}
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                        isInvalid={!!errors.name}
+                        required
+                    />
+                    {errors.name && (
+                        <Form.Text className="text-danger">
+                            {errors.name.message}
+                        </Form.Text>
+                    )}
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Add Card
+                </Button>
+            </Form>
+        </div>
+    );
+};
+
+export default AddCreditCard;
--- src/Pages/Account/Components/CreditCards.tsx
+++ src/Pages/Account/Components/CreditCards.tsx
@@ -2,10 +2,14 @@ import ListGroup from "react-bootstrap/ListGroup";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
 import useUser from "../../../Hooks/useUser";
+import useModal from "../../../Hooks/useModal";
+import AddCreditCard from "../../../Components/Modal/AddCreditCard/AddCreditCard";
 
 const UserDetails = () => {
     const { user, getUserCreditCards } = useUser();
     const creditCards = getUserCreditCards(user!.id);
+
+    const { setContent, setTitle } = useModal();
 
     return (
         <>
@@ -26,12 +30,21 @@ const UserDetails = () => {
                         {" - "}
                         {c.expirationDate}
                         {" - "}
-                        {c.cvv}
+                        {c.cvc}
                     </ListGroup.Item>
                 ))}
             </ListGroup>
-            <div className="d-grid gap-2">
-                <Button variant="primary" size="lg">
+            <div className="d-grid gap-2 mt-2">
+                <Button
+                    variant="primary"
+                    size="lg"
+                    onClick={() => {
+                        if (setTitle && setContent) {
+                            setTitle("Add Credit Card");
+                            setContent(<AddCreditCard />);
+                        }
+                    }}
+                >
                     Add Credit Card
                 </Button>
             </div>
